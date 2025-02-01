1bd6cc66e5008d26e289a2b9674477e186c7a689
Juliana Caedo <julianapcaedo@iskolarngbayan.pup.edu.ph>
2025-02-02 06:16:42 +0800

    Frontend:
    Bulletin
    Directory
    RecyclableMaterials
    WasteSegregationSearch

--- app/src/main/java/com/appdev/group3/ecollect/Bulletin.java
+++ app/src/main/java/com/appdev/group3/ecollect/Bulletin.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class UserLogin extends AppCompatActivity {
+public class Bulletin extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_user_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.admin_noacctxt), (v, insets) -> {
+        setContentView(R.layout.activity_bulletin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
